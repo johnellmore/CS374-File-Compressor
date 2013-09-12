86a8c5a86a3ebd7629ffd708f929d804fa782e7e
Fraser Mince <bfm09a@acu.edu>
2013-09-11 19:43:08 -0500

    Create functions to compress and decompress.

--- compress.cpp
+++ compress.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
+string compress(string input){
+	return input;
+}
+
+string decompress(string input){
+	return input;
+}
+
 int main(){
-	cout << "hello world" << endl;
+	string input = "TOBEORNOTTOBEORTOBEORNOT";
+	string output = decompress(compress(input));
+	assert(input == output);
+	cout << "done" << endl;
 }
